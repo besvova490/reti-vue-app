36168ddfe0803becd840d22f9f3829d4ddfdb49b
volodymyr-coax-dev <volodymyr.sen@coaxsoft.com>
2023-10-08 15:27:20 +0300

    main added metting room page

--- src/components/BaseVideoBlock.vue
+++ src/components/BaseVideoBlock.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="reti-video-block">
-    <video src=""></video>
-    <span class="reti-video-block__label">
-      Abraham
+    <video v-if="!!src" :src="src"></video>
+    <div v-else class="reti-video-block__icon">
+      <font-awesome-icon v-if="!avatar" :icon="faUser" />
+      <img :src="avatar" :alt="`user logo avatar ${label}`">
+    </div>
+    <span v-if="!!label" class="reti-video-block__label">
+      {{ label }}
     </span>
   </div>
 </template>
 
 <script>
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 export default {
-  name: "BaseVideoBlock"
+  name: "BaseVideoBlock",
+  props: {
+    src: {
+      type: String,
+      default: ""
+    },
+    label: {
+      type: String,
+      default: ""
+    },
+    avatar: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    faUser: () => faUser
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .reti-video-block {
   width: 100%;
-  height: 100%;
+  height: 300px;
 
   border-radius: 12px;
   background: rgba(188, 188, 211, 0.40);
@@ -24,6 +47,39 @@ export default {
 
   overflow: hidden;
   position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__icon {
+    width: 68px;
+    height: 68px;
+
+    overflow: hidden;
+    border-radius: 50%;
+    background: #F5F5FD;
+    box-shadow: 0px 0px 8px 4px rgba(86, 57, 203, 0.50);
+
+    color: #8871E6;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      width: 20px;
+      height: 20px;
+      flex: 0 0 20px;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+
+      object-fit: cover;
+    }
+  }
 
   &__label {
     position: absolute;
--- src/views/meeting-room/components/MeetingRoomInner.vue
+++ src/views/meeting-room/components/MeetingRoomInner.vue
@@ -1,26 +1,89 @@
 <template>
-  <div class="reti-meeting-room__inner">
-    <base-video-block></base-video-block>
-    <base-video-block></base-video-block>
-    <base-video-block></base-video-block>
-  </div>
+  <transition-group
+    class="reti-meeting-room__inner"
+    tag="div"
+    name="reti-meeting-room__inner"
+    :data-videos-count="guests.length >= 3 ? 3 : guests.length"
+    >
+    <base-video-block
+      :key="item.id"
+      v-for="(item) in guests"
+      :label="item.userName"
+      :avatar="item.avatar"
+    ></base-video-block>
+  </transition-group>
 </template>
 
 <script>
+import { faker } from "@faker-js/faker";
 import BaseVideoBlock from "@/components/BaseVideoBlock.vue";
 
 export default {
   name: "MeetingRoomInner",
   components: {
     BaseVideoBlock
+  },
+  data () {
+    return {
+      guests: [
+        {
+          id: faker.datatype.uuid(),
+          userName: faker.internet.displayName(),
+          avatar: `https://picsum.photos/200/300?${faker.datatype.uuid()}`,
+          isMuted: false,
+          isVideoOn: true
+        }
+      ]
+    };
   }
 };
 </script>
 
-<style lang="scss" scoped>
-.reti-meeting-room__inner {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 24px
+<style lang="scss">
+.reti-meeting-room {
+
+  &__inner-enter-from,
+  &__inner-leave-to {
+    opacity: 0;
+    transform: scale(0.7);
+  }
+
+  &__inner {
+    display: grid;
+    gap: 24px;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    &:where([data-videos-count="1"]) {
+      align-items: center;
+      justify-content: center;
+      grid-template-columns: repeat(1, 1fr);
+
+      & > :nth-child(1) {
+        grid-column: 1/-1;
+        height: 600px;
+      }
+    }
+
+    &:where([data-videos-count="2"]) {
+      align-items: center;
+      justify-content: center;
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    &:where([data-videos-count="3"]) {
+      grid-template-columns: repeat(6, 1fr);
+      grid-auto-rows: max-content;
+
+      & > :nth-child(1) { grid-column: span 3; }
+      & > :nth-child(2) {grid-column: span 3; }
+
+      & > * {  grid-column: span 2; }
+    }
+  }
+
+  .reti-video-block {
+    transition: all .3s;
+  }
 }
 </style>
